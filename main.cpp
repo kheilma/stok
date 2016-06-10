ffa1cf352de4c5cc093d1e0ccc531a69f3276e00
kheilma <kyleheilman@Kyles-MBP.lfytina2.metronetinc.com>
2016-06-10 16:15:11 -0400

    fixing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
-#include <conio.h>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Options.hpp>
+#include <string>
+#include <cstring>
+using namespace std;
 
 
 string fetchData(string url) {
-    System.Net.WebClient wc = new System.Net.WebClient();
-    byte[] raw = wc.DownloadData(url);
-    
-    string data = System.Text.Encoding.UTF8.GetString(raw);
-    }
+    curlpp::Cleanup myCleanup;
+    std::ostringstream os;
+    os << curlpp::options::Url(string(url));
+    string completed = os.str();
+}
     
     void main() {
         string read;
         read = fetchData("http://www.foxnews.com/sports/2016/06/10/fans-from-around-world-bid-final-farewell-to-muhammad-ali.html");
         cout << read << "\n";
     }
+
